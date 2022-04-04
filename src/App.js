1767d7ff2a003acd57df3c89f4c38b28ca88bed8
Jairo <pachecocamposjairo@gmail.com>
2022-04-03 19:36:22 -0600

    admin

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
 import Landpage from "./layouts/Landpage/Landpage.js";
 import { Route, Routes } from "react-router-dom";
-import Login from "./layouts/login/Login.js"
+import Login from "./layouts/login/Login.js";
+import AdminView from "./layouts/admin/AdminView"
 
 function App() {
   return (
@@ -9,6 +10,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Landpage />} />
         <Route path="/login" element={<Login />} />
+        <Route path="admin" element={<AdminView></AdminView>}>
+          <Route index element={<p>Profile</p>} />
+          {/* <Route path="profile" element={<Login />} />
+          <Route path="users" element={<UserManagement />} />
+          <Route path="products" element={<Products />} /> */}
+        </Route>
       </Routes>
     </div>
   );
