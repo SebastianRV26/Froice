4c36a004ea3b9414a405b62eff51b61192c0c0af
Fran Soto <fran-soto@hotmail.es>
2022-05-01 17:38:57 -0600

    Mejoras paginación y modificar usuarios

--- src/components/Table/TableComponent.jsx
+++ src/components/Table/TableComponent.jsx
@@ -1,66 +1,34 @@
 import { endAt, getDocs, limit, query, startAt } from "firebase/firestore";
 import React, { useCallback, useEffect, useState } from "react";
-import Pagination from "react-bootstrap/Pagination";
 import Button from "react-bootstrap/Button";
 import classes from "./TableComponent.module.css";
 import Placeholder from "react-bootstrap/Placeholder";
 import sadFace from "../../assets/sad-face.svg";
 import Image from "react-bootstrap/Image";
+import FirestorePagination from "../FirestorePagination/FirestorePagination";
 
 const TableComponent = (props) => {
-  const pageSize = 2;
   const [list, setList] = useState([]);
-  const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(true);
 
-  const previousDisabled = page === 1;
-  const nextDisabled = list.length <= pageSize;
-
-  const dbCollection = props.collection;
   const actions = props.actions;
 
-  const fetchData = useCallback(
-    async (queryOptions = []) => {
-      setLoading(true);
-      try {
-        const q = query(
-          dbCollection,
-          limit(pageSize + 1),
-          ...props.queryOptions,
-          ...queryOptions
-        );
-        const querySnapshot = await getDocs(q);
-        let items = [];
-        querySnapshot.forEach((doc) => {
-          items.push({ id: doc.id, ...doc.data() });
-        });
-        setList(items);
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setLoading(false);
-      }
-    },
-    [dbCollection, props.queryOptions]
-  );
+  const onFetchStart = useCallback(() => {
+    setLoading(true);
+  }, []);
 
-  useEffect(() => {
-    fetchData();
-    setPage(1);
-  }, [fetchData]);
+  const onFetchFinish = useCallback((list) => {
+    setList(list);
+    setLoading(false);
+  }, []);
 
-  const showNextPage = () => {
-    fetchData([startAt(list[list.length - 1])]);
-    setPage((page) => page + 1);
-  };
-
-  const showPreviousPage = () => {
-    fetchData([endAt(list[0])]);
-    setPage((page) => page - 1);
-  };
+  const onFetchError = useCallback((error) => {
+    setLoading(false);
+    console.log(error);
+  }, []);
 
   const renderRows = () => {
-    return list.slice(0, pageSize).map((data) => (
+    return list.map((data) => (
       <tr key={data.id}>
         {props.columns.map((colData) => (
           <td key={colData.key} data-label={colData.label}>
@@ -116,40 +84,39 @@ const TableComponent = (props) => {
     return rows;
   };
 
-  if (!loading && list.length === 0) {
-    return (
-      <div className={classes.empty}>
-        <Image src={sadFace} alt="Cara triste" />
-        <h2>No se ha encontrado ningún resultado</h2>
-        <p>Pruebe agregar datos o ajustar el filtro de búsqueda</p>
-      </div>
-    );
-  }
-
   return (
     <>
-      <table>
-        <thead>
-          <tr>
-            {props.columns.map((colData) => (
-              <th key={colData.key}>{colData.label}</th>
-            ))}
-            {actions && <th>Acciones</th>}
-          </tr>
-        </thead>
-        <tbody>
-          {!loading && renderRows()}
-          {loading && renderLoadingRows()}
-        </tbody>
-      </table>
-      <Pagination className={classes.pagination}>
-        <Pagination.Prev
-          disabled={previousDisabled}
-          onClick={showPreviousPage}
-        />
-        <Pagination.Item active>{page}</Pagination.Item>
-        <Pagination.Next disabled={nextDisabled} onClick={showNextPage} />
-      </Pagination>
+      {(loading || (!loading && list.length > 0)) && (
+        <table>
+          <thead>
+            <tr>
+              {props.columns.map((colData) => (
+                <th key={colData.key}>{colData.label}</th>
+              ))}
+              {actions && <th>Acciones</th>}
+            </tr>
+          </thead>
+          <tbody>
+            {!loading && renderRows()}
+            {loading && renderLoadingRows()}
+          </tbody>
+        </table>
+      )}
+      {!loading && list.length === 0 && (
+        <div className={classes.empty}>
+          <Image src={sadFace} alt="Cara triste" />
+          <h2>No se ha encontrado ningún resultado</h2>
+          <p>Pruebe agregar datos o ajustar el filtro de búsqueda</p>
+        </div>
+      )}
+      <FirestorePagination
+        pageSize={5}
+        collection={props.collection}
+        queryOptions={props.queryOptions}
+        onFetchStart={onFetchStart}
+        onFetchFinish={onFetchFinish}
+        onFetchError={onFetchError}
+      ></FirestorePagination>
     </>
   );
 };
