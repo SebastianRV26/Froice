a1906fe61cf69c847e0472268a9f1a304c7275ca
Fran Soto <fran-soto@hotmail.es>
2022-05-15 17:45:31 -0600

    Scroll infinito y opiniones de un persona

--- src/components/Routes/AuthenticatedRoute.jsx
+++ src/components/Routes/AuthenticatedRoute.jsx
@@ -6,17 +6,22 @@ const AuthenticatedRoute = ({ component: RouteComponent, requieredRole }) => {
   const authData = useAuth();
   const isAuthenticated = authData.user !== null;
 
-  if (
-    isAuthenticated && (authData.role === "admin" || authData.role === requieredRole)
-  ) {
+  // Primero ver si está autenticado, sino vayase al login
+  if (isAuthenticated) {
+    // Segundo es ver si el correo está verificado, sino esta verificado mandelo al verificar
     if (location.pathname === "/verify-email") {
       if (authData.user.emailVerified) {
         <Navigate to="/dashboard" replace={true} />;
       } else {
-        return <RouteComponent />;
+        return <RouteComponent />; // Mantener en verify email
       }
     } else if (authData.user.emailVerified) {
-      return <RouteComponent />;
+      // Si esta verificado, revisar si es admin o service y controlar acceso
+      if (requieredRole === "both" || requieredRole === authData.role) {
+        return <RouteComponent />;
+      }
+      // Sino cumple con los requisitos llevarlo a la home page
+      <Navigate to="/dashboard" replace={true} />;
     } else {
       return <Navigate to="/verify-email" replace={true} />;
     }
